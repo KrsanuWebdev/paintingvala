620c9fdd637366818e30a86db7112a24e2bc51e0
KrsanuWebdev <sonu.singh@thetechnopartner.com>
2024-10-26 13:46:41 +0530

    checking cicd

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,24 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Context, Handler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
+let server: Handler ;
 
-async function bootstrap() {
+async function bootstrapServerless() {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
+}
+
+export const handler: Handler = async (event: any, context: Context) => {
+  server = server ?? (await bootstrapServerless());
+  return server(event, context);
+};
+
+
+async function bootstrap() { 
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService)
   const port = Number(configService.get('PORT') ?? '3500'); 
